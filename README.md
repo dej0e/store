09dd6a7f5bdc6e729c229e95053ff2d6cd507862
dej0e <dejoejohn@gmail.com>
2023-11-24 21:47:24 -0500

    Adds models, views and homepage.

--- README.md
+++
@@ -1,2 +0,0 @@
-# store
- IS601-Final Project
--- store/finalproject/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-
-urlpatterns = [
-    path('', views.home, name='home'),
-    path('about/', views.aboutPage, name='about'),
-    path('item/<slug:slug>/', views.product_detail, name='product_detail'),
-]
